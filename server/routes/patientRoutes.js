2ad1f990e9d9994c3f176caf35ac12409425da73
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-05-28 01:00:55 +0530

    book appointments

--- server/routes/patientRoutes.js
+++ server/routes/patientRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const Patient = require('../models/patient')
+const Doctor = require('../models/doctor')
+const Appointment = require('../models/appointment')
 const passwordHasher = require('../utils/password')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const isAuth = require('../middlewares/isAuth')
 const isPatient = require('../middlewares/isPatient')
+const { default: mongoose } = require('mongoose')
 
 router.post('/register', async (req, res) => {
     try {
@@ -53,6 +56,50 @@ router.post('/login', async (req, res) => {
 router.get('/patient-dashboard', isAuth, isPatient, async (req, res) => {
     console.log(req.user);
     res.send('Protected Routes')
+})
+
+
+router.post('/book-appointment', isAuth, isPatient, async (req, res) => {
+    try {
+        console.log(req.user);
+        const { patient, doctor, date, time, status } = req.body
+        const getPatient = await Patient.findById(patient);
+        if (!getPatient) {
+            return res.status(404).json({ error: 'Patient not found' });
+        }
+        const getDoctor = await Doctor.findById(doctor);
+        if (!getDoctor) {
+            return res.status(404).json({ error: 'Patient not found' });
+        }
+        if (!patient || !doctor || !date || !time || !status) {
+            res.status(400).json({ message: 'All fields are required' })
+        }
+        const newAppointment = new Appointment({
+            patient, doctor, date, time, status
+        })
+
+        await newAppointment.save()
+        return res.status(201).json({ message: 'New patient record created successfully', appointment: newAppointment })
+    } catch (error) {
+        console.error('Error while booking appointment', err);
+    }
+})
+
+
+router.get('/get-appointment/:id', isAuth, isPatient, async (req, res) => {
+    try {
+        const { id } = req.params
+        const appointment = await Appointment.find({ patient: id })
+            .populate('patient', 'name place email')
+            .populate('doctor', 'firstName lastName email department')
+            .exec();
+        if (!appointment) {
+            return res.status(404).json({ error: 'Appointments not found' });
+        }
+        return res.status(200).json({ message: 'All appoinments fetched successfully', appointment })
+    } catch (error) {
+        console.error('Error while fetching appointments', error);
+    }
 })
 
 module.exports = router
