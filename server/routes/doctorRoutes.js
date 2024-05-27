36f231e770b8323c5ecc061f7b3a1303961e194b
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-05-28 00:09:55 +0530

    doc login and register

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -11,7 +11,7 @@ const isAuth = require('../middlewares/isAuth')
 router.post('/register', async (req, res) => {
     try {
         const { email, password, firstName, lastName, phoneNumber, department } = req.body;
-        if (!email || !password || !name || !gender || !phoneNumber || !address || !place) {
+        if (!email || !password || !firstName || !lastName || !phoneNumber || !department) {
             res.status(400).json({ message: 'All fields are required' })
         }
         const checkDoctor = await Doctor.findOne({ email })
@@ -20,7 +20,7 @@ router.post('/register', async (req, res) => {
         }
         const hashPassword = passwordHasher.createHashPassword(password)
         const newDoctor = new Doctor({
-            email, password: hashPassword, name, gender, phoneNumber, address, place
+            email, password: hashPassword, firstName, lastName, phoneNumber, department
         })
         await newDoctor.save()
         return res.status(201).json({ message: 'New Doctor record created successfully' })
@@ -28,5 +28,26 @@ router.post('/register', async (req, res) => {
         console.error('Error while registering Doctor', error);
     }
 })
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const doctor = await Doctor.findOne({ email })
+        if (!doctor) {
+            return res.status(400).json({ message: 'Doctor does not exist, register as new Doctor' })
+        }
+        const checkPassword = passwordHasher.comparePassword(password, doctor.password)
+        if (!checkPassword) {
+            return res.status(400).json({ message: 'Incorrect password' })
+
+        }
+        const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+        res.setHeader('Authorization', `Bearer ${token}`)
+        return res.status(200).json({ message: 'Doctor logged in successfully', doctor, token })
+    } catch (error) {
+        console.error('Error while login Doctor');
+    }
+})
+
 
 module.exports = router
