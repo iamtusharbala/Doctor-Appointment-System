1645c695064aa1d849bda0b21644fd25b63a3fab
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-05-28 20:33:53 +0530

    patient : view and book appt

--- client/src/components/AppointmentCard/AppointmentCard.jsx
+++ client/src/components/AppointmentCard/AppointmentCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const AppointmentCard = () => {
+const AppointmentCard = ({ doctor, department, time, date, status }) => {
     return (
-        <div>AppointmentCard</div>
+        <div>
+            <div className="card mb-3">
+                <div className="card-body">
+                    <h5 className="card-title">{doctor}</h5>
+                    <p className="card-text"><small className="text-body-muted">Department : {department}</small></p>
+                    <p className="card-text"><small className="text-body-secondary">Time: {time}</small></p>
+                    <p className="card-text"><small className="text-body-secondary">Date: {date}</small></p>
+                    <p className="card-text"><small className="text-body-secondary">Status: {status}</small></p>
+                    <a href="#" className="btn btn-primary">Edit Appointment</a>
+                    <a href="#" className="btn btn-danger">Delete Appointment</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/User/GetAppointments.jsx
+++ client/src/pages/User/GetAppointments.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import AppointmentCard from '../../components/AppointmentCard/AppointmentCard'
 
 const GetAppointments = ({ userId, token }) => {
     // /get-appointment/:id'
@@ -17,10 +18,14 @@ const GetAppointments = ({ userId, token }) => {
             }
         });
         setAppointments(response.data.appointment)
+        console.log(response.data.appointment)
         // console.log(response.data.appointment)
+        // appointments.map((appt)=>app)
     }
     return (
-        <div>GetAppointments</div>
+        <div className='container mt-5'>
+            {appointments && appointments.map((appt) => <AppointmentCard key={appt._id} doctor={appt.doctor.firstName + " " + appt.doctor.lastName} department={appt.doctor.department} time={appt.time} date={appt.date} status={appt.status} />)}
+        </div>
     )
 }
 
